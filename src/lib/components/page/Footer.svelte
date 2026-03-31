d7307b1e8b18dd71bc51478cab0f4ca6c985e743
kevinkeyssx <keyss1700@gmail.com>
2026-03-31 17:50:37 -0300

    Terms page linked

--- src/lib/components/page/Footer.svelte
+++ src/lib/components/page/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="w-full mt-auto relative flex flex-col items-center justify-center border-t border-gray-200 dark:border-slate-900/10 dark:dark:border-white/10 bg-white/30 dark:bg-slate-950/30 backdrop-blur-md py-6 transition-colors duration-300">
+<footer class="w-full mt-auto relative flex flex-col items-center justify-center border-t border-gray-200 dark:border-white/10 bg-white/30 dark:bg-slate-950/30 backdrop-blur-md py-6 transition-colors duration-300">
     <div class="relative z-10 flex flex-col sm:flex-row items-center justify-between w-full max-w-7xl px-6 gap-4">
 		<!-- Marca y Copyright -->
 		<div class="flex items-center gap-2">
@@ -11,11 +11,11 @@
 
 		<!-- Enlaces de Relleno -->
         <div class="flex items-center gap-6 text-sm font-semibold text-slate-500 dark:text-slate-400">
-            <a href="https://github.com/KevinKeyssx/EcoSync-Front" target="_blank" rel="noopener noreferrer" class="hover:text-emerald-600 dark:hover:text-emerald-500 dark:hover:text-emerald-700 dark:dark:hover:text-emerald-400 transition-colors">
+            <a href="https://github.com/KevinKeyssx/EcoSync-Front" target="_blank" rel="noopener noreferrer" class="hover:text-emerald-600 dark:hover:text-emerald-400 transition-colors">
                 GitHub Repo
             </a>
 
-            <a href="#terminos" class="hover:text-emerald-600 dark:hover:text-emerald-500 dark:hover:text-emerald-700 dark:dark:hover:text-emerald-400 transition-colors">
+            <a href="/terminos" class="hover:text-emerald-600 dark:hover:text-emerald-400 transition-colors">
                 Términos
             </a>
         </div>
